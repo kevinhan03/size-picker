1d317c7bac0e56ef1890f46bc46875d9ee9f2a5c
sunwoo0813 <sunwoo0813k@gmail.com>
2026-05-07 02:07:01 +0900

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import type { ReactNode } from "react";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { ClientProviders } from "../src/components/ClientProviders";
 import "./globals.css";
 
@@ -47,6 +48,7 @@ export default async function RootLayout({
           {modal}
         </ClientProviders>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
